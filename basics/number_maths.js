70919e1a21cd5f7edaa234d50c637c2e8d7ad633
Sumitm45 <161346685+Sumitm45@users.noreply.github.com>
2025-07-10 11:19:09 +0000

    number and math program and notes

---
+++ basics/number_maths.js
@@ -0,0 +1,35 @@
+const score=300;
+const balance=new Number(1000);
+//console.log(balance.toString().length);
+//const s=new String("msd");
+//console.log(s.length);
+//console.log(score.toFixed(1));
+/*const anotherint=264.888;
+console.log(anotherint.toPrecision(4));
+const num=26.8465962
+console.log(num.toPrecision(3)) ;
+const rs=1000000000000;
+console.log(rs.toLocaleString())
+console.log(rs.toLocaleString('en-in'));*/
+
+
+//------------------------MATH---------------------------------//
+/*console.log(Math.abs(-10));
+console.log(Math.ceil(10.5));
+
+console.log(Math.floor(10.5));
+console.log(Math.round(10.5));  */
+/*console.log(Math.max(25,30,15,85))
+console.log(Math.min(15,25,85,65,11));*/
+/*console.log(Math.random()); // returns a random number between 0 and 1
+console.log(Math.floor(Math.random()*10)+1); */
+// returns a random number between 0 and 10   
+//returns value between 1 and 10
+/*const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min); 
+// returns a random number between 10 and 20*/
+const min=100;
+const max=200;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+//returns a random number between 100 and 200
