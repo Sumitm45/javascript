5391f1507b652f732d6e505235b72158ab8c08c2
Sumitm45 <161346685+Sumitm45@users.noreply.github.com>
2025-07-10 09:29:56 +0000

    String operation

---
+++ basics/test.js
@@ -0,0 +1,32 @@
+const name="virat"
+const repocount=35;
+//console.log(`my name is ${name} and my repo count is ${repocount}`);
+const gameName = new String("real24");
+//console.log(gameName[0]);
+
+//console.log(gameName.toUpperCase());
+//console.log(gameName.__proto__);
+
+//console.log(gameName.charAt(5));
+//console.log(gameName.indexOf("2"));
+//console.log(gameName.substring(1,5));
+//console.log(gameName.slice(1,3));
+//console.log(gameName.slice(-5,-3));
+ //const anstring="    rohit   ";
+ //console.log(anstring.trim());
+ 
+//const nstring="virat-is-great"
+//console.log(nstring.split("-"));
+//const fstring="https://virat.com/hitesh%20choudhary"
+/*console.log(fstring.replace("virat","rohit"));
+console.log(fstring.includes('msd'));
+console.log(fstring.includes('rohit'));*/
+console.log(gameName.bold());
+
+
+
+
+
+
+
+
