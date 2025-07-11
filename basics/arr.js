8522f861a2f9cddded4591c4898dc1b8d14f411a
Sumitm45 <161346685+Sumitm45@users.noreply.github.com>
2025-07-11 16:53:21 +0000

    arr changes

---
+++ basics/arr.js
@@ -0,0 +1,4 @@
+let arr=[265, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];     
+let narr=arr.splice(1,5);
+console.log(narr); // [3, 4, 5, 6, 7]
+console.log(arr); // [265, 8, 9, 10, 11, 12, 13, 14, 15]
