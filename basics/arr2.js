8e505df53df4e80a651164276cd58d36105733ae
Sumitm45 <161346685+Sumitm45@users.noreply.github.com>
2025-07-11 17:53:03 +0000

    more array function

---
+++ basics/arr2.js
@@ -0,0 +1,22 @@
+//let heroes=["rohit","virat","dhoni","sachin"];  
+//let heroes2=["ABD","Raina","Hardik","Bhuvi"];
+//let all_heroes=heroes.concat(heroes2);
+//console.log(all_heroes);
+// ["rohit", "virat", "dhoni", "sachin", "ABD", "Raina", "Hardik", "Bhuvi"]
+//const new_heroes=[...heroes,...heroes2];
+//console.log(new_heroes);
+// ["rohit", "virat", "dhoni", "sachin", "ABD", "Raina", "Hardik", "Bhuvi"]
+//let loop=[12, 13,[14, [15, 16, 17]], [18, 19,20], 21, 22, 23,];
+//let loop_sorted=loop.flat(Infinity);
+//console.log(loop_sorted);
+// [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+//console.log(Array.isArray("Virat Kohli")); // false
+//let none=Array.from("Virat Kohli");
+//console.log(none); // ["V", "i", "r", "a", "K", "o", "h", "l", "i"]
+let score1=264;
+let score2=183;
+let score3=149;
+
+let f=Array.of(score1, score2, score3);
+console.log(f); // [264, 183, 149]
+
