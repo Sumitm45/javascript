3ab0b943f483e4a321c3b0b852e749410513f555
Sumitm45 <161346685+Sumitm45@users.noreply.github.com>
2025-07-11 17:03:12 +0000

    some more

--- basics/arr.js
+++ basics/arr.js
@@ -1,4 +1,28 @@
 let arr=[265, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];     
-let narr=arr.splice(1,5);
-console.log(narr); // [3, 4, 5, 6, 7]
-console.log(arr); // [265, 8, 9, 10, 11, 12, 13, 14, 15]
+//let narr=arr.splice(1,5);
+//console.log(narr); // [3, 4, 5, 6, 7]
+//console.log(arr); // [265, 8, 9, 10, 11, 12, 13, 14, 15]
+let farr=arr.slice(1,5);
+console.log(farr); // [3, 4, 5, 6]
+console.log(arr); // [265, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+arr.push(289);
+console.log(arr);
+// [265, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 289]
+arr.pop();
+console.log(arr);
+// [265, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+arr.unshift(23);
+console.log(arr);
+// [23, 265, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+arr.shift();
+console.log(arr);   
+// [265, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+let arr1=arr.concat([1,2,3]); 
+console.log(arr1);
+// [265, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 1, 2, 3]
+arr.join();
+// "265,3,4,5,6,7,8,9,10,11,12,13,14,15"
+console.log(arr.join());
+// "265,3,4,5,6,7,8,9,10,11,12,13,14,15"
+
+
