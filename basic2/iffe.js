efdc06d53d623bc5c2c7d0cf66a2c9d2f3ddfdd3
Sumitm45 <161346685+Sumitm45@users.noreply.github.com>
2025-07-14 17:24:38 +0000

    iife

---
+++ basic2/iffe.js
@@ -0,0 +1,17 @@
+//immediately invoked function
+
+(function virat(){
+ console.log(`virat is a good cricketer`);
+})();
+// iife is used to avoid polluting the global scope and to create a private scope
+
+
+
+
+
+((name)=>{
+    console.log(`This is an arrow function${"virat"}`);
+})();
+
+
+
