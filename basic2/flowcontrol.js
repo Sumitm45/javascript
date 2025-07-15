e5e6c3935aeb2bf463fc365eeeaff8b9b7a63bc5
Sumitm45 <161346685+Sumitm45@users.noreply.github.com>
2025-07-15 17:44:24 +0000

    switch

---
+++ basic2/flowcontrol.js
@@ -0,0 +1,15 @@
+const player_name="virat"
+switch(player_name){
+    case "rohit" :
+        console.log("rohit is greatest");
+         break;
+    case "dhoni" :
+         console.log("dhoni is greatest");
+         break;
+    case "hardik" :
+        console.log("rohit is greatest");
+        break;
+    case "virat" :
+         console.log("virat is greatest");
+
+}
