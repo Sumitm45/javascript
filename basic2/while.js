20223f1e1af9e97df18b2f76abacd28ebbfb6b39
Sumitm45 <161346685+Sumitm45@users.noreply.github.com>
2025-07-16 20:09:57 +0000

    while and do while

---
+++ basic2/while.js
@@ -0,0 +1,19 @@
+let virat=0;
+while(virat<10){
+    console.log(`value of virta is ${virat}`)
+    virat++;
+}
+let myArray=["abd","stokes","root","smith"]
+let i=0;
+while (i<myArray.length) {
+    const ele=myArray[i];
+    console.log(ele);
+    
+    i++;
+}
+
+let score=12;
+do{
+    console.log(`score is ${score}`);
+    score++
+}while(score<11)
