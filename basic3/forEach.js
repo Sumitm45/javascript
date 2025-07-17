dff9cd0b4d4c39fe15facfd66dcdec38d41c61df
Sumitm45 <161346685+Sumitm45@users.noreply.github.com>
2025-07-17 19:48:58 +0000

    for each loop

---
+++ basic3/forEach.js
@@ -0,0 +1,34 @@
+// for each loop
+const arr=["MI","CSK","RCB","DC","LSG"]
+//arr.forEach(function(item){
+  //  console.log(item);
+//})
+//arr.forEach( (element)=>{
+ //console.log(element);
+ 
+//})
+
+//arr.forEach((item,index,arr)=>{
+  // console.log(item,index,arr);
+   
+//})
+const myPlayer=[
+{playerName:"Hitman",
+  bestScore:264
+},
+{
+  playerName:"King Kohli",
+  bestScore:183
+},
+{
+  playerName:"Thala",
+  bestScore:184
+}
+
+]
+myPlayer.forEach((item)=>{
+  console.log(item.playerName);
+  console.log(item.bestScore );
+  
+  
+})
